0d58cc7f8cf89eac1b0aa5bd33978261d7924900
Vamsi Krishna Puttamraju <vamsikrishnaputtamraju@gmail.com>
2024-12-23 12:31:01 +0530

    scriptfetch.js

--- scriptfetch.js
+++ scriptfetch.js
@@ -1,38 +1,23 @@
-// function fetchUserData() {
-//     return new Promise((resolve) => {
-//         setTimeout(() => resolve('User data received'), 1000);
-//     });
-// }
-
-// function fetchPosts() {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => resolve('Posts data received'), 5000);
-//     });
-// }
-
-// fetchUserData()
-//     .then((userData) => {
-//         console.log(userData);
-//         return fetchPosts();
-//     })
-//     .then((postsData) => {
-//         console.log(postsData);
-//     })
-//     .catch((error) => {
-//         console.error('Error:', error);
-//     });
-
-
-
-
-async function getUserAndPosts() {
-    try {
-        const userData = await fetchUserData();
-        console.log(userData);
-        const postsData = await fetchPosts();
-        console.log(postsData);
-    } catch (error) {
-
-    }
+function fetchUserData() {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve('User data received'), 1000);
+    });
 }
-getUserAndPosts();
+
+function fetchPosts() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve('Posts data received'), 5000);
+    });
+}
+
+fetchUserData()
+    .then((userData) => {
+        console.log(userData);
+        return fetchPosts();
+    })
+    .then((postsData) => {
+        console.log(postsData);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
